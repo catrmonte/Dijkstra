8de56169ac81fda8ea2e9a3f4919eaf9790b10ce
catrmonte <59862274+catrmonte@users.noreply.github.com>
2020-03-20 19:41:49 -0700

    Add files via upload

---
+++ Scripts/SteeringOutput.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SteeringOutput
+{
+    public Vector3 linear;
+    public float angular; // degrees please
+}
